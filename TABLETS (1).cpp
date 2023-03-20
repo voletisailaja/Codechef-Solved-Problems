9bda133ad89db486bf9029e5000584bc1e691816
Voleti Sailaja <81481090+voletisailaja@users.noreply.github.com>
2023-03-20 16:55:53 +0530

    Add files via upload

---
+++ TABLETS (1).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	int x,y;
+	cin>>x>>y;
+	if(y>=x*3) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	
+	}
+	return 0;
+}
