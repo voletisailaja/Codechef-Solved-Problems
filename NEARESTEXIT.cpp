77194bcb8f2b5a3681b9b6a81c731e7eea4738b0
Voleti Sailaja <81481090+voletisailaja@users.noreply.github.com>
2023-03-20 16:01:32 +0530

    Add files via upload

---
+++ NEARESTEXIT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<=50) cout<<"LEFT"<<endl;
+	    else cout<<"RIGHT"<<endl;
+	}
+	return 0;
+}
